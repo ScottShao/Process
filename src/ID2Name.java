6555f130e7163a5b54296bb58b694c304e1185eb
Zhaoyang-Shao <zhaoyang.shao@Ibm.com>
2017-04-05 13:47:04 -0400

    id to name java

---
+++ src/ID2Name.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ID2Name {
+
+	public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException, IOException {
+		if (args.length < 1) {
+			System.out.println("Not enough arguments, please retry");
+			System.exit(1);
+		}
+		Map<Integer, String> entityMapping = loadMapping("entityMapping.txt");
+		Map<Integer, String> predicateMapping = loadMapping("predicatesMapping.txt");
+		
+		try (BufferedReader br = new BufferedReader(new FileReader("args[0]"))) {
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				String[] words = line.split(" ");
+				System.out.println(entityMapping.get(Integer.parseInt(words[0])) + " " + entityMapping.get(Integer.parseInt(words[1])) + " " +
+									predicateMapping.get(Integer.parseInt(words[2])));
+			}
+		} 
+	}
+	
+	
+	private static Map<Integer, String> loadMapping(String fileName) throws FileNotFoundException, IOException {
+		Map<Integer, String> res = new HashMap<>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				String[] words = line.split(" ");
+				res.put(Integer.parseInt(words[1]), words[0]);
+			}
+		}
+		
+		return res;
+	}
+
+}
